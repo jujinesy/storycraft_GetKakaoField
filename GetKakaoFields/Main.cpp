44dacacccdd418ffcc6a02fdd4357266fa73dcee
jujinesy <jujinesy@gmail.com>
2021-05-16 17:26:03 +0900

    2105161725

--- GetKakaoFields/Main.cpp
+++ GetKakaoFields/Main.cpp
@@ -10,13 +10,10 @@ void Start();
 DWORD dwKakao = 0;
 
 DWORD Kakao_return_1 = 0;
-BYTE jmp[] = { 0xe9,0x00,0x00,0x00,0x00, 0x90 };
+BYTE jmp[8] = { 0xe9,0x00,0x00,0x00,0x00,0x90,0x90,0x90 };
 
-char* idFormat = "Id> %d\n";
-char* statusFormat = "Status> %hu\n";
-char* nameFormat = "Packet> %.10s\n";
-char* bodyTypeFormat = "Body type> %hhx\n";
-char* bodyLengthFormat = "Body length> %d\n";
+char* pathFormat = "Path> %S\n";
+char* Keyformat = "SQLite op> %s\n\n";
 
 bool bCompare(const BYTE* pData, const BYTE* bMask, const char* szMask)
 {
@@ -46,25 +43,12 @@ BOOL MemoryEdit(VOID *lpMem, VOID *lpSrc, DWORD len)
 }
 DWORD Hook(LPVOID lpFunction)
 {
-	DWORD dwAddr = Kakao_return_1 - 6;
+	DWORD dwAddr = Kakao_return_1 - 8;
 	DWORD dwCalc = ((DWORD)lpFunction - dwAddr - 5);
 
 	memcpy(&jmp[1], &dwCalc, 4);
-	WriteProcessMemory(GetCurrentProcess(), (LPVOID)dwAddr, jmp, sizeof(jmp), 0);
+	WriteProcessMemory(GetCurrentProcess(), (LPVOID)dwAddr, jmp, 8, 0);
 	return dwAddr;
-}
-
-void PrintBodyHex(char* body, int size) {
-
-	printf("Body>");
-
-	int i;
-	for (i = 0; i < size; i++) {
-		printf(" %02hhX", (unsigned char) body[i]);
-	}
-
-	printf("\n\n");
-
 }
 
 void __declspec(naked) Kakao_hook_1()
@@ -72,57 +56,25 @@ void __declspec(naked) Kakao_hook_1()
 	__asm
 	{
 		pushad
-
-		mov ecx, [ebx + 4]
-		mov ebx, [ecx]
+		push dword ptr[edi]
+		push pathFormat
+		call printf
+		add esp, 8
+		popad
 
 		pushad
-		push[ebx]
-		push idFormat
+		push esi
+		push Keyformat
 		call printf
 		add esp, 8
-
-		push[ebx + 4]
-		push statusFormat
-		call printf
-		add esp, 8
-
-		add ebx, 6
-		push ebx
-		sub ebx, 6
-		push nameFormat
-		call printf
-		add esp, 8
-
-		push[ebx + 17]
-		push bodyTypeFormat
-		call printf
-		add esp, 8
-
-		push[ebx + 18]
-		push bodyLengthFormat
-		call printf
-		add esp, 8
-
-		push[ebx + 18]
-		add ebx, 22
-		push ebx
-		sub ebx, 22
-		call PrintBodyHex
-		add esp, 8
-
 		popad
 
-		popad
-
-		mov eax, dword ptr ds : [ebx]
-		mov ecx, ebx
-		push 1
+		mov byte ptr ss : [ebp - 4] , 9
+		cmp dword ptr ss : [ebp - 18] , 0
 
 		jmp[Kakao_return_1]
 	}
 }
-
 void Start()
 {
 	DWORD dwSize = 0x1000000;
@@ -134,9 +86,9 @@ void Start()
 	} while (!dwKakao);
 	Sleep(100);
 	printf("Kakao: %x\n", dwKakao);
-	dwAddress = FindPattern(dwKakao, dwSize, (PBYTE)"\x8B\xCE\xE8\x00\x00\x00\x00\x8B\x03\x8B\xCB\x6A\x01", "xxx????xxxxxx");
+	dwAddress = FindPattern(dwKakao, dwSize, (PBYTE)"\x50\xE8\xFF\xFF\xFF\xFF\xC6\x45\xFC\x09\x83\x7D\xE8\x00", "xx????xxxxxxxx");
 	printf("Address: %x\n", dwAddress);
-	Kakao_return_1 = dwAddress + 13;
+	Kakao_return_1 = dwAddress + 14;
 	Hook(Kakao_hook_1);
 }
 BOOL APIENTRY DllMain(HMODULE hModul, DWORD ul_reason_for_ca, LPVOID lpReserve)
--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 
 GetKakaoFields...
 
-![Imgur](https://i.imgur.com/NdtAeXz.png)
+
+![script2](https://cloud.githubusercontent.com/assets/16377085/11927425/c9377b08-a80f-11e5-8e64-d3736ce3aef4.png)
